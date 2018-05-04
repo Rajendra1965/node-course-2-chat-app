03333e1511bd6972cb754a7d4c1cc6555103269e
rajendra <r_k_sahu@yahoo.com>
2018-05-04 13:07:28 +0530

    Modified for port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 
@@ -9,6 +10,9 @@ console.log(publicPath);
 var app = express();
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log(`Server is up on port ${port}`);
+app.listen(port, host, function() {
+    console.log("Server started......." + "at Port: " + port + " on host: " + host);
 });
+// app.listen(3000, () => {
+//     console.log(`Server is up on port ${port}`);
+// });
